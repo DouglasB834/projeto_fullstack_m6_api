e8e6db03418c2d9f65aa23cd35182c797b19719e
douglasB834 <douglas_borges09@hotmail.com>
2023-03-22 13:30:08 -0300

    feat: add regex paramas id

---
+++ back/src/middleware/veifyContactIdParams.middleware.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express";
+import { AppDataSource } from "../data-source";
+import { Users } from "../entities/usersEntity";
+import AppError from "../errors/AppError";
+
+export const verifyContactIdParameter = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const userRepo = AppDataSource.getRepository(Users);
+  const { id } = req.params;
+  const regexExp =
+    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+  if (!regexExp.test(id)) {
+    throw new AppError("Invalid input, id must be a valid uuid", 401);
+  }
+  return next();
+};
--- back/src/routes/contact.routes.ts
+++ back/src/routes/contact.routes.ts
@@ -6,7 +6,7 @@ import {
   retrieverContactcController,
   updadeContactController,
 } from "../controllers";
-import { verifyAuth } from "../middleware";
+import { verifyAuth, verifyContactIdParameter } from "../middleware";
 import { verifyContactMiddlware } from "../middleware/verifyContact.middleware";
 import {
   contactsSchema,
@@ -23,11 +23,22 @@ contatcRountes.post(
 );
 contatcRountes.patch(
   "/contact/:id",
+  verifyContactIdParameter,
   verifyAuth,
   verifyContactMiddlware(contactsUpdateSchema),
   updadeContactController
 );
 
-contatcRountes.get("/contact/:id", verifyAuth, retrieverContactcController);
+contatcRountes.get(
+  "/contact/:id",
+  verifyContactIdParameter,
+  verifyAuth,
+  retrieverContactcController
+);
 contatcRountes.get("/contact", verifyAuth, listContactstController);
-contatcRountes.delete("/contact/:id", verifyAuth, deleteContactController);
+contatcRountes.delete(
+  "/contact/:id",
+  verifyContactIdParameter,
+  verifyAuth,
+  deleteContactController
+);
